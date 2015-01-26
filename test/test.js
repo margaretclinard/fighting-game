a92797582887f4743bba0bc5ed920cf3dd95cc05
Margaret Clinard <margaret.clinard@gmail.com>
2015-01-26 09:41:31 -0600

    added tests to test.js

--- test/test.js
+++ test/test.js
@@ -4,13 +4,32 @@
 'use strict';
 
 var expect = require('chai').expect,
-    Game   = require(process.cwd() + '/lib/Game');
+    Game   = require(process.cwd() + '/lib/Game'),
+    Weapon = require(process.cwd() + '/lib/Weapon'),
+    Player = require(process.cwd() + '/lib/Player');
 
-describe('Game', function () {
-  describe('constructor', function () {
-    it('should have a default name', function() {
+describe('Player', function () {
+  describe('constructor for name with no argument', function () {
+    it('should have a default name', function () {
       var player = new Player();
-      expect(player.name).to.equal('Player 1');
+      expect(player.name).to.equal('Player');
+    });
+  });
+   describe('constructor for name with argument', function () {
+    it('should have the argument name', function () {
+      var player = new Player({name: 'Mario'});
+      expect(player.name).to.equal('Mario');
+    });
+  });
+});
+
+describe('Weapon', function () {
+  describe('constructor for weapon', function () {
+    it('should accept an argument', function () {
+      var weapon = new Weapon({name: 'Sword', damage: 0.06});
+      expect(weapon.name).to.equal('Sword');
+      expect(weapon.ammo).to.equal(0);
+      expect(weapon.accuracy).to.equal(0.94);
     });
   });
 });
